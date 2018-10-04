0e55cbd4a6f2bed776c705ea3dec3e43ac8d2ec0
yongfei.zong <yongfei.zong@ucarinc.com>
2018-10-05 00:04:42 +0800

    删除多余的代码

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,14 +1,15 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    <div>hello home</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'Home'
 }
 </script>
 
 <style>
+
 </style>
