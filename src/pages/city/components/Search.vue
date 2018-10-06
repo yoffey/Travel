36ea13a137b614587c06f5c22b56278936d6aa9d
yongfei.zong <yongfei.zong@ucarinc.com>
2018-10-06 21:49:12 +0800

    search logic

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,65 @@
 <template>
-  <div class="search">
-    <input type="text" placeholder="输入城市名或拼音" class="search-input">
+  <div>
+    <div class="search">
+      <input v-model="keyword" type="text" placeholder="输入城市名或拼音" class="search-input">
+    </div>
+    <div class="search-content" ref="wrapper">
+      <ul>
+        <li class="search-item border-bottom" v-for="city in list" :key="city.id">
+          {{city.name}}
+        </li>
+        <li class="search-item border-bottom" v-show="hasNoData">
+          没有找到匹配数据
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.wrapper)
+  },
+  watch: {
+    keyword () {
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach(city => {
+            if (city.spell.indexOf(this.keyword) > -1 || city.name.indexOf(this.keyword) > -1) {
+              result.push(city)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  }
 }
 </script>
 
@@ -25,4 +78,18 @@ export default {
       text-align: center
       border-radius: .06rem
       color: #666
+  .search-content
+    z-index: 1
+    overflow: hidden
+    position: absolute
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
+    background: #eee
+    .search-item
+      line-height: .62rem
+      padding-left: .2rem
+      color: #666
+      background: #fff
 </style>
